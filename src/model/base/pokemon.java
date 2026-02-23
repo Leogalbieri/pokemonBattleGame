0cb25a325069200e1bce348fcf751a6d5ac1ab10
Leogalbieri <leogalbieri@outlook.com>
2026-02-22 22:24:58 -0300

    Pokemon and Blastoise fix

---
+++ src/model/base/pokemon.java
@@ -0,0 +1,51 @@
+package model.base;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+public abstract class Pokemon {
+
+    protected String nome;
+    protected String elemento;
+    protected int vida, vidaMax;
+    protected String spritePath;
+
+    public Pokemon(String nome, String elemento, int vida, int vidaMax) {
+        this.nome = nome;
+        this.elemento = elemento;
+        this.vida = vida;
+        this.vidaMax = vidaMax;
+    }
+
+    public abstract String evoluir();
+    public abstract List<Ataque> getAtaques();
+    public abstract List<String> fraquezas();
+    public abstract List<String> resistencias();
+
+    public void receberDano(int dano) {
+        this.vida -= dano;
+
+        if (this.vida <= 0) {
+            this.vida = 0;
+        }
+    }
+
+    public boolean desmaiado() {
+        return this.vida <= 0;
+    }
+
+    public String getNome() { return nome; }
+    public void setNome(String nome) { this.nome = nome; }
+
+    public String getElemento() { return elemento; }
+    public void setElemento(String elemento) { this.elemento = elemento; }
+
+    public int getVida() { return vida; }
+    public void setVida(int vida) { this.vida = vida; }
+
+    public int getVidaMax() { return vidaMax; }
+    public void setVidaMax(int vidaMax) { this.vidaMax = vidaMax; }
+
+    public String getSpritePath() { return spritePath; }
+}
---
+++ src/model/elementos/agua/Blastoise.java
@@ -0,0 +1,72 @@
+package model.elementos.agua;
+
+import java.util.List;
+import model.base.Ataque;
+import model.base.Pokemon;
+
+public class Blastoise extends Agua {
+    private List<Ataque> ataques;
+    private int estagio;
+
+    public Blastoise(int estagio) {
+        super("Blastoise", 127, 127);
+        this.estagio = estagio;
+        configurarEstagio(this.estagio);
+    }
+
+    public String evoluir() {
+        String nomePokemon = getNome();
+        if (this.estagio < 2) {
+            this.estagio++;
+            configurarEstagio(this.estagio);
+            return nomePokemon + " evoluiu para " + getNome() + "!";
+        }
+        return null;
+    }
+
+    private void configurarEstagio(int estagio) {
+        if (estagio == 0) {
+            setNome("Squirtle");
+            setVidaMax(127);
+            this.spritePath = "/res/sprites/squirtle.png";
+            ataques = List.of(
+                    new Ataque("Tackle", "Normal", 40),
+                    new Ataque("Water Gun", "Agua", 40),
+                    new Ataque("Bubble", "Agua", 30),
+                    new Ataque("Aqua Tail", "Agua", 35)
+            );
+        } else if (estagio == 1) {
+            setNome("Wartortle");
+            setVidaMax(153);
+            this.spritePath = "/res/sprites/wartortle.png";
+            ataques = List.of(
+                    new Ataque("Water Gun", "Agua", 60),
+                    new Ataque("Water Pulse", "Agua", 80),
+                    new Ataque("Aqua Jet", "Agua", 55),
+                    new Ataque("Dig", "Terrestre", 100)
+            );
+        } else  if (estagio == 2) {
+            setNome("Blastoise");
+            setVidaMax(238);
+            this.spritePath = "/res/sprites/blastoise.png";
+            ataques = List.of(
+                    new Ataque("Hydro Pump", "Agua", 130),
+                    new Ataque("Flash Cannon", "Aço", 110),
+                    new Ataque("Surf", "Agua", 90),
+                    new Ataque("Hydro Cannon", "Agua", 80)
+            );
+        }
+        setVida(getVidaMax());
+    }
+
+    public List<Ataque> getAtaques() {
+        return ataques;
+    }
+
+    public int realizarAtaque(int index) {
+        Ataque escolhido = ataques.get(index);
+        setElemento(escolhido.getElemento());
+        return escolhido.getDano();
+    }
+
+}
